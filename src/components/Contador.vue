80cd424fa9986d508f7eb0b55c6679f3fd90743d
Yensi Jesús González Nova <yensigonzalesnova55@gmail.com>
2024-08-10 20:30:41 +0200

    Creando un componente de Contador

---
+++ src/components/Contador.vue
@@ -0,0 +1,23 @@
+<template>
+  <h1>Hola Mundo</h1>
+  <p>{{ contador }}</p>
+  <button @click="incrementar">Incrementar</button>
+  <button @click="disminuir">Disminuir</button>
+</template>
+
+<script setup>
+  import {ref} from 'vue';
+  const contador = ref(0);
+  const incrementar = () => {
+    contador.value++;
+  }
+  const disminuir = () => {
+    contador.value--;
+  }
+</script>
+
+<style scoped>
+  p {
+    color: red;
+  } 
+</style>
